8bb69a6a039e140a7c6a50a62a344f62d97f7eac
Gay Hui Jie <huijie_1995@hotmail.com>
2020-05-21 14:55:40 +0800

    db

--- index.php
+++ index.php
@@ -2,6 +2,32 @@
 <?php include('templates/header.php');?>
 
 Index Page, Home has an active class
+<?php
+include 'db_connection.php';
 
+$conn = OpenCon();
+
+echo "Connected Successfully";
+
+CloseCon($conn);
+
+?>
+
+
+<form action="insert.php" method="post">
+    <p>
+        <label for="firstName">First Name:</label>
+        <input type="text" name="first_name" id="firstName">
+    </p>
+    <p>
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="last_name" id="lastName">
+    </p>
+    <p>
+        <label for="emailAddress">Email Address:</label>
+        <input type="text" name="email" id="emailAddress">
+    </p>
+    <input type="submit" value="Submit">
+</form>
 <!--Footer-->
 <?php include('templates/footer.php');?>
