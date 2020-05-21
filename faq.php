393f4add8c66fa930cd30cb74c799fd00f97068b
Ganize <bruce_gan95@hotmail.com>
2020-05-21 14:50:45 +0800

    added css for faq and story

--- faq.php
+++ faq.php
@@ -1,7 +1,97 @@
 <!-- Header-->
 <?php include('templates/header.php');?>
+<link rel="stylesheet" href="css/faq.css" type="text/css">
+<div class="words"> 
+    Frequently Answered Questions
+</div>
+<hr>
 
-Index Page, Home has an active class
+<ul>
+  <li>Are you halal certified?</li>
+  <div class="breakline">
+  We are not halal certified but our ingredients consists no pork, no lard
+  </div>
+
+  <li>What kind of cakes do you do?</li>
+ 
+  <div class="breakline">
+  We provide buttercream cakes, fondant cakes, fresh cream cakes, icing print cakes</div>
+
+  <li>Are you able to recreate a similar cake that I provide?</li>
+  <div class="breakline">
+  We do respect copyrights and will only take inspiration from references
+  </div>
+
+  <li>Do you do dessert tables?</li>
+  <div class="breakline">
+  Yes we do provide dessert table packages, do drop us an email at butterytablebakery@gmail.com
+  </div>
+
+  <li>Can we mix the flavours for different tiers?</li>
+  <div class="breakline">
+  <p>Yes you can!</p>
+  <p>For 2 flavours -  Additional $10</p>
+  <p>For 3 flavours - Additional $15</p>
+  </div>
+
+  <li>What will be provided for cake orders?</li>
+  <div class="breakline">
+  We provide a basic happy birthday topper, plastic knife & candles!
+  </div>
+
+  <li>Do you provide customised cake toppers?</li>
+  <div class="breakline">
+  We only provide a basic happy birthday topper. However, we do provide premium toppers at a custom rate. Do send us an inquiry via our order/enquire form to check out the pricing
+  </div>
+
+  <li>DDo you do delivery?</li>
+  <div class="breakline">
+  <p>Yes! Our delivery fees starts from $15 depending on the amount you are ordering.</p>
+  <p>Deliveries to Sentosa/Tuas will incur an additional $10.</p>
+  <p>Delivery days & time: Monday to Sunday: 10 am - 10 pm</p>
+  </div>
+
+  <li>Can we self-collect?</li>
+  <div class="breakline">
+  Self-collection is available, we will arrange the pick-up time and address with you
+  </div>
+
+  <li>How do I transport my cake home?</li>
+  <div class="breakline">
+  <p>We recommend a car as a means of transport</p>
+  <p>Please place cakes on a flat surface during transportation, at the leg space of the passenger seat</p>
+  <p>The car should be as cool as possible when transporting the cake and drive straight to the location if possible</p>
+  </div>
+
+  <li>How do I store my cake?</li>
+  <div class="breakline">
+  <p>Always refrigerate your cake and defrost 1 hour prior to consumption</p>
+  <p>Cakes can be placed in an air-conditioned environment for a maximum of 2 hours</p>
+  </div>
+
+  <li>How long in advance should I place my order?</li>
+  <div class="breakline">
+  As all our bakes are made-to-order, we recommend you to send in your orders at least 1 - 2 weeks in advance. For urgent cases, please place orders 3 days in advance depending on slot availability.
+  </div>
+
+  <li>When and how do I make payment?</li>
+  <div class="breakline">
+  <p>Payment is to be made through PayNow/Paylah/Bank transfer upon receiving the order confirmation email with the provided bank account details.</p>
+  <p>**Your order will only be confirmed once full payment is received.</p>
+  </div>
+  
+  <li>Changes</li>
+  <div class="breakline">
+  We only allow changes up to 1 week before the order date.
+  </div>
+
+  <li>Cancellation & Refund Policy</li>
+  <div class="breakline">
+  <p>Full refunds will be provided up to 2 weeks before the order date. Thereafter, we will not issue any refunds.</p>
+  <p>**Refunds will be made within a period of 4 weeks from the date of acknowledgement.</p>
+  </div>
+
+</ul>
 
 <!--Footer-->
 <?php include('templates/footer.php');?>
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!-- Header-->
 <?php include('templates/header.php');?>
 
-Index Page, Home has an active class
+
+
+<a href="https://www.facebook.com/butterytable/"><i class="fa fa-facebook-square"></i></a>
+<a href="https://www.instagram.com/butterytablebakery/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
 
 <!--Footer-->
 <?php include('templates/footer.php');?>
+
