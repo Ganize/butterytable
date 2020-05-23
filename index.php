6fa4535964dff67be1ffd779a4ba8d22a16ebcb4
Gay Hui Jie <huijie_1995@hotmail.com>
2020-05-23 11:39:43 +0800

    Login and DB

--- index.php
+++ index.php
@@ -3,38 +3,40 @@
 
 
 <?php
-include 'db_connection.php';
-=======
-
-
-<a href="https://www.facebook.com/butterytable/"><i class="fa fa-facebook-square"></i></a>
-<a href="https://www.instagram.com/butterytablebakery/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
->>>>>>> 393f4add8c66fa930cd30cb74c799fd00f97068b
-
-$conn = OpenCon();
-
-echo "Connected Successfully";
-
-CloseCon($conn);
+	include 'db_connection.php';
+	$conn = openCon();
+	if($conn)
+	{
+		echo "Connected Successfully";
+    
+	}
+	
+	closeCon($conn);
 
 ?>
 
 
-<form action="insert.php" method="post">
-    <p>
-        <label for="firstName">First Name:</label>
-        <input type="text" name="first_name" id="firstName">
-    </p>
-    <p>
-        <label for="lastName">Last Name:</label>
-        <input type="text" name="last_name" id="lastName">
-    </p>
-    <p>
-        <label for="emailAddress">Email Address:</label>
-        <input type="text" name="email" id="emailAddress">
-    </p>
-    <input type="submit" value="Submit">
-</form>
+<a href="https://www.facebook.com/butterytable/"><i class="fa fa-facebook-square"></i></a>
+<a href="https://www.instagram.com/butterytablebakery/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+
+<div class="form-container">
+    <form action="/butterytable/login/insert.php" method="post">
+        <div class="form-group">
+            <label for="user_name">Username</label>
+            <input type="text" name="user_name" id="user_name">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="text" name="password" id="password">
+        </div>
+        <div class="form-group">
+            <label for="email">Email Address</label>
+            <input type="text" name="email" id="email">
+       </div>
+        <button class="btn" type="submit" name="register_user" >Save</button>
+    </form>
+</div>
+
 <!--Footer-->
 <?php include('templates/footer.php');?>
 
