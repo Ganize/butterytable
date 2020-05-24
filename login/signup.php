b6e9557f8f78ebf2b93fb5c9a1207e2aa41beb05
Gay Hui Jie <huijie_1995@hotmail.com>
2020-05-24 17:21:00 +0800

    Test
    Signed-off-by: Gay Hui Jie <huijie_1995@hotmail.com>

---
+++ login/signup.php
@@ -0,0 +1,30 @@
+<!-- Header-->
+
+<?php include('../templates/header.php');?>
+
+<div class="container">
+    <div class="form-container">
+        <form action="../includes/signup.inc.php" method="post">
+            <div class="form-group">
+                <label for="user_name">Username</label>
+                <input type="text" name="user_name" id="user_name">
+            </div>
+            <div class="form-group">
+                <label for="email">Email Address</label>
+                <input type="text" name="email" id="email">
+           </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div class="form-group">
+                <label for="password-repeat">Repeat Password</label>
+                <input type="password" name="password-repeat" id="password">
+            </div>
+          
+            <button class="btn" type="submit" name="register_user" >Register</button>
+        </form>
+    </div>
+</div>
+<!--Footer-->
+<?php include('../templates/footer.php');?>
