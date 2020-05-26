f91579cd3bf169d4c70068fda2093c308e34ff47
Gay Hui Jie <huijie_1995@hotmail.com>
2020-05-26 17:54:31 +0800

    mail
    Signed-off-by: Gay Hui Jie <huijie_1995@hotmail.com>

--- contact-us.php
+++ contact-us.php
@@ -59,9 +59,65 @@
 		// 			}
 		// 		}				
 		// 	}
-		// }
-	
+		// }	
 ?>
+<?php 
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'PHPMailer/src/Exception.php';
+	require 'PHPMailer/src/PHPMailer.php';
+	require 'PHPMailer/src/SMTP.php';
+
+	//require 'vendor/autoload.php';
+
+	// Instantiation and passing `true` enables exceptions
+	$mail = new PHPMailer(true);
+
+	try
+	{
+	//Server settings
+	    $mail->isSMTP();                                            // Send using SMTP
+		$mail->Host = 'smtp.gmail.com';						        // Set the SMTP server to send through
+	    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+	    $mail->Username   = 'ghuijie95@gmail.com';                  // SMTP username
+	    $mail->Password   = 'Ghj160795@.com';                       // SMTP password
+	    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+	    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+	    //Recipients
+	    $mail->setFrom('from@example.com', 'Mailer');
+	    $mail->addAddress('ghuijie1995@gmail.com', 'Joe User');     // Add a recipient
+	 
+
+	    // Content
+	    $mail->isHTML(true);                                  // Set email format to HTML
+	    $mail->Subject = 'Here is the subject';
+	    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+	    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    	$mail->send();
+
+		echo 'Message has been sent';
+	}
+	catch(Exception $e)
+	{
+		 echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	}
+
+?>
+<form method="post" name="myemailform" action="includes/email.inc.php">
+	<p>
+		<label for='name'>Enter Name: </label><br>
+		<input type="text" name="name"/>
+	</p>
+	<p>
+		<label for='email'>Enter Email Address:</label><br>
+		<input type="text" name="email"/>
+	</p>
+	<button name="submit">Submit</button>
+
+</form>
 </div>
 <script>
 	jQuery(document).ready(function($){
